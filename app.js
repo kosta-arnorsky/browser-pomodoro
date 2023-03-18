35ce2b949c09d43ee5ae9b8a6bc8f992f65ec5b6
Kosta_Arnorsky <aleksandr.castillo@gmail.com>
2023-03-18 15:21:53 -0400

    Hide the set timer field when Pomadoro is running in preparation to the hover functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function () {
-    var template = '<div style="all:initial;position:fixed;z-index:2147483647;top:55px;right:8px;background:#fff;display:flex;align-items:center"><input id="_pomTimeInput" type="number" maxlength="2" style="all:initial;max-width:36px;padding:1px 2px;border:1px inset #767676"> <input id="_pomStartStopButton" type="button" style="all:initial;padding:3px 8px;border:none;border-radius:8px;margin:0 3px;color:#fff;background-color:#04aa6d"><div id="_pomCloseButton" title="Close Pomodoro" style="width:10px;height:10px;border:3px solid red;border-radius:100%;background:linear-gradient(-45deg,transparent 45%,#fff 45%,#fff 55%,transparent 55%),linear-gradient(45deg,transparent 45%,#fff 45Q,#fff 55%,transparent 55%),linear-gradient(45deg,transparent 46%,#fff 46%,#fff 56%,transparent 56%);background-color:red;cursor:pointer"></div></div>';
+    var template = '<div style="all:initial;position:fixed;z-index:2147483647;top:55px;right:8px;background:#fff;display:flex;align-items:center"><div id="_pomSetTime"><input id="_pomTimeInput" type="number" maxlength="2" style="all:initial;max-width:36px;padding:1px 2px;border:1px inset #767676"> <input id="_pomStartButton" type="button" value="Start" style="all:initial;padding:3px 8px;border:none;border-radius:8px;margin:0 3px;color:#fff;background-color:#04aa6d"></div><div id="_pomRunning" style="display:none;width:20px;height:20px;border:3px solid #04aa6d;border-radius:100%;background-color:#04aa6d"></div><div id="_pomCloseButton" title="Close Pomodoro" style="width:10px;height:10px;border:3px solid red;border-radius:100%;background:linear-gradient(-45deg,transparent 45%,#fff 45%,#fff 55%,transparent 55%),linear-gradient(45deg,transparent 45%,#fff 45Q,#fff 55%,transparent 55%),linear-gradient(45deg,transparent 46%,#fff 46%,#fff 56%,transparent 56%);background-color:red;cursor:pointer"></div></div>';
     var state = Object.freeze({
         setTime: 1,
         running: 2
@@ -15,8 +15,10 @@
     var _appResources = {
         domElements: {
             containerDiv: null,
+            setTimeDiv: null,
             timeInput: null,
-            startStopButton: null,
+            startButton: null,
+            runningIndicatorDiv: null,
             closeButton: null
         },
         timeoutHandler: null,
@@ -29,13 +31,15 @@
 
         // Save refs to DOM elements
         _appResources.domElements.containerDiv = container;
+        _appResources.domElements.setTimeDiv = container.querySelector("#_pomSetTime");
         _appResources.domElements.timeInput = container.querySelector("#_pomTimeInput");
-        _appResources.domElements.startStopButton = container.querySelector("#_pomStartStopButton");
+        _appResources.domElements.startButton = container.querySelector("#_pomStartButton");
+        _appResources.domElements.runningIndicatorDiv = container.querySelector("#_pomRunning");
         _appResources.domElements.closeButton = container.querySelector("#_pomCloseButton");
 
         // Wire events
         _appResources.domElements.timeInput.addEventListener("change", onChangeTime);
-        _appResources.domElements.startStopButton.addEventListener("click", onStartStopClick);
+        _appResources.domElements.startButton.addEventListener("click", onStartClick);
         _appResources.domElements.closeButton.addEventListener("click", onCloseClick);
 
         setState(defaultAppState);
@@ -49,21 +53,18 @@
     function render() {
         switch (_appState.state) {
             case state.setTime:
-                _appResources.domElements.containerDiv.removeAttribute("title");
                 _appResources.domElements.timeInput.value = _appState.minutes.toString();
-                _appResources.domElements.timeInput.removeAttribute("disabled");
-                _appResources.domElements.startStopButton.setAttribute("value", "Start");
+                _appResources.domElements.setTimeDiv.style.display = "block";
+                _appResources.domElements.runningIndicatorDiv.style.display = "none";
+                _appResources.domElements.closeButton.style.display = "block";
                 break;
             case state.running:
+                _appResources.domElements.setTimeDiv.style.display = "none";
                 var timePassed = Date.now() - _appState.setAt;
-
                 var secondsLeft = _appState.minutes * 60 - convertMillisecondsToSeconds(timePassed);
-                _appResources.domElements.containerDiv.setAttribute("title", formatTimeMinSec(secondsLeft) + " left");
-
-                var minutesLeft = _appState.minutes - convertMillisecondsToMinutes(timePassed);
-                _appResources.domElements.timeInput.value = minutesLeft.toString();
-                _appResources.domElements.timeInput.setAttribute("disabled", "disabled");
-                _appResources.domElements.startStopButton.setAttribute("value", "Stop");
+                _appResources.domElements.runningIndicatorDiv.setAttribute("title", formatTimeMinSec(secondsLeft) + " left");
+                _appResources.domElements.runningIndicatorDiv.style.display = "block";
+                _appResources.domElements.closeButton.style.display = "none";
                 break;
         }
     }
@@ -72,7 +73,7 @@
         clearTimeout(_appResources.timeoutHandler);
         clearInterval(_appResources.intervalHandler);
         _appResources.domElements.timeInput.removeEventListener("change", onChangeTime);
-        _appResources.domElements.startStopButton.removeEventListener("click", onStartStopClick);
+        _appResources.domElements.startButton.removeEventListener("click", onStartClick);
         _appResources.domElements.closeButton.removeEventListener("click", onCloseClick);
         _appResources.domElements.containerDiv.remove();
 
@@ -92,25 +93,22 @@
         }
     }
 
-    function onStartStopClick() {
-        switch (_appState.state) {
-            case state.setTime:
-                _appResources.timeoutHandler = setTimeout(onTimeout, convertMinutesToMilliseconds(_appState.minutes));
-                _appResources.intervalHandler = setInterval(onInterval, 1000);
+    function onStartClick() {
+        _appResources.timeoutHandler = setTimeout(onTimeout, convertMinutesToMilliseconds(_appState.minutes));
+        _appResources.intervalHandler = setInterval(onInterval, 1000);
 
-                var newState = {
-                    state: state.running,
-                    setAt: Date.now()
-                };
-                setState(newState);
-                break;
-            case state.running:
-                clearTimeout(_appResources.timeoutHandler);
-                clearInterval(_appResources.intervalHandler);
+        var newState = {
+            state: state.running,
+            setAt: Date.now()
+        };
+        setState(newState);
+    }
 
-                setState(defaultAppState);
-                break;
-        }
+    function onStopClick() {
+        clearTimeout(_appResources.timeoutHandler);
+        clearInterval(_appResources.intervalHandler);
+
+        setState(defaultAppState);
     }
 
     function onTimeout() {
@@ -178,10 +176,6 @@
 
     function convertMillisecondsToSeconds(milliseconds) {
         return Math.floor(milliseconds / 1000);
-    }
-
-    function convertMillisecondsToMinutes(milliseconds) {
-        return Math.floor(milliseconds / (60 * 1000));
     }
 
     init();
