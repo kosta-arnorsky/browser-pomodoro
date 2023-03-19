1c4225ab159d3d48e0c01373da9aa712beca42e3
Kosta_Arnorsky <aleksandr.castillo@gmail.com>
2023-03-18 21:01:44 -0400

    Fix an error in Firefox browser

--- node-scripts/build.mjs
+++ node-scripts/build.mjs
@@ -2,7 +2,11 @@ import { minify } from "minify";
 import { writeFile } from "fs/promises";
 
 try {
-    const minifiedApp = await minify("./app.js");
+    let minifiedApp = await minify("./app.js");
+    if (minifiedApp.startsWith("!function()") && minifiedApp.endsWith("}();")) {
+        // Have no idea why minify changes the function declaration
+        minifiedApp = `(${minifiedApp.slice(1, minifiedApp.length - 4)}})()`;
+    }
     await writeFile("./bundle.txt", "javascript: " + encodeURI(minifiedApp));
 } catch (error) {
     console.error(error);
